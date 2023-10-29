cb4a319ab44e1ea596101f85e199f74e1cbb4ed0
Kanyisa Ntombini <kanyisa.ntombini@gmail.com>
2023-10-29 13:50:33 +0200

    first commit

---
+++ src/Patient.java
@@ -0,0 +1,9 @@
+public class Patient {
+    private String name;
+    private int idNumber;
+
+    public Patient(String name, int idNumber) {
+        this.name = name;
+        this.idNumber = idNumber;
+    }
+}
